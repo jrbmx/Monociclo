3b1284a9785a05ca9f581a62cae308330c1740b3
jrbmx <146797068+jrbmx@users.noreply.github.com>
2024-07-01 09:04:12 -0600

    Update sll1bit.v

--- sll1bit/sll1bit.v
+++ sll1bit/sll1bit.v
@@ -1,3 +1,17 @@
+/*	Grupo: 5CV3 	
+Proyecto: sll1bit	
+Archivo: sll1bit.v
+Equipo: 6
+Integrantes: 
+Díaz Ortiz Brandon Aldair			
+González Rosales Brenda Yareth
+Hernández Suarez Diego Armando			
+Pérez Aguilar Dulce Evelyn 
+Romero Barrientos Jonathan Rubén
+Descripción: Hace un desplazamiento hacia la izquierda en un bit
+de la entrada_i y lo guarda en una salida_o
+*/
+
 module sll1bit(
 	input [31:0] entrada_i,
 	output [31:0] salida_o
